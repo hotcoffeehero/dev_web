c46792e62458a5719619d4477c0b9ee930c95f18
hotcoffeehero <hotcoffeehero@gmail.com>
2021-01-11 17:32:02 +0900

    17: Create & Update profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,9 +4,11 @@ const auth = require('../../middleware/auth')
 
 const Profile = require('../../models/Profile')
 const User = require('../../models/User')
+const { check, validationResult } = require('express-validator')
 
+//=====================================================================
 // @route   GET api/profile/me
-// @desc    get your profile
+// @desc    Get logged in user's profile
 // access   Private
 router.get('/me', auth, async (req, res) => {
   try {
@@ -26,5 +28,86 @@ router.get('/me', auth, async (req, res) => {
     res.status(500).send('Server Error')
   }
 })
+//=====================================================================
+
+// ====================================================================
+// @route   POST api/profile
+// @desc    Create or update user profile
+// access   Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', 'Status is required').not().isEmpty(),
+      check('skills', 'Skills are required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body
+
+    //Build Profile Object to insert into DB
+    const profileFields = {}
+    profileFields.user = req.user.id
+    if (company) profileFields.company = company
+    if (website) profileFields.website = website
+    if (location) profileFields.location = location
+    if (bio) profileFields.bio = bio
+    if (status) profileFields.status = status
+    if (githubusername) profileFields.githubusername = githubusername
+    if (skills) {
+      profileFields.skills = skills.split(',').map((skill) => skill.trim())
+    }
+    // console.log(profileFields.skills)
+    // res.send('Hello')
+
+    //Build Social media presence object
+    profileFields.social = {}
+    if (youtube) profileFields.social.youtube = youtube
+    if (twitter) profileFields.social.twitter = twitter
+    if (facebook) profileFields.social.facebook = facebook
+    if (linkedin) profileFields.social.linkedin = linkedin
+    if (instagram) profileFields.social.instagram = instagram
+
+    //
+    try {
+      let profile = await Profile.findOne({ user: req.user.id })
+
+      //If a profile already exists, show it
+      if (profile) {
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        )
+        return res.json(profile)
+      }
+
+      //Otherwise, create it
+      profile = new Profile(profileFields)
+      await profile.save()
+      res.json(profile)
+    } catch (error) {}
+  }
+)
+
+// ====================================================================
 
 module.exports = router
