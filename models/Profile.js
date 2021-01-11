e1f0b4d52b4a891fb8d642fa4d66b95a6d56591c
hotcoffeehero <hotcoffeehero@gmail.com>
2021-01-11 16:09:13 +0900

    16: Get Current User Profile

--- models/Profile.js
+++ models/Profile.js
@@ -110,4 +110,4 @@ const ProfileSchema = new mongoose.Schema({
   },
 })
 
-module.exports = Profile = mongoose.model('profile', Profile)
+module.exports = Profile = mongoose.model('profile', ProfileSchema)
