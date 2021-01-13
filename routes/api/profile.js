83e1891ee673478d7d615981a62a66f983822b6c
hotcoffeehero <hotcoffeehero@gmail.com>
2021-01-14 06:49:09 +0900

    Delete Profile Experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -216,4 +216,23 @@ router.put(
   }
 )
 
+//=====================================================================
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    delete profile experience
+// access   private
+router.delete('/experience/:exp_id', authWare, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id })
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id)
+    profile.experience.splice(removeIndex, 1)
+    await profile.save()
+    res.json(profile)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).send('Sever Error')
+  }
+})
+
 module.exports = router
