56b8ae0ccb0b9ca0f02f1d2af2b501494fbe3708
hotcoffeehero <hotcoffeehero@gmail.com>
2021-01-15 06:25:05 +0900

    22: Add & Delete Profile Education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -235,4 +235,79 @@ router.delete('/experience/:exp_id', authWare, async (req, res) => {
   }
 })
 
+//=====================================================================
+// @route   PUT api/profile/education
+// @desc    add profile education
+// access   private
+router.put(
+  '/education',
+  [
+    auth,
+    [
+      check('school', 'School is Required').not().isEmpty(),
+      check('degree', 'Degree is Required').not().isEmpty(),
+      check('fieldofstudy', 'Field of Study is Required').not().isEmpty(),
+      check('from', 'From date is Required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    } = req.body
+
+    const newEdu = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    }
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id })
+
+      profile.education.unshift(newEdu)
+
+      await profile.save()
+
+      res.json(profile)
+    } catch (err) {
+      console.error(err.message)
+      res.status(500).send('Server Error')
+    }
+  }
+)
+
+//=====================================================================
+// @route   DELETE api/profile/education/:edu_id
+// @desc    delete profile education
+// access   private
+router.delete('/education/:edu_id', authWare, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id })
+    const removeIndex = profile.education
+      .map((item) => item.id)
+      .indexOf(req.params.edu_id)
+    profile.education.splice(removeIndex, 1)
+    await profile.save()
+    res.json(profile)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).send('Sever Error')
+  }
+})
+
 module.exports = router
