77876b04eb641e1ae09a065f0fb13bfb149951b8
hotcoffeehero <hotcoffeehero@gmail.com>
2021-01-26 17:27:14 +0900

    Get User Profile

--- dev_head/routes/api/profile.js
+++ dev_head/routes/api/profile.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const router = express.Router()
+const authWare = require('../../middleware/auth')
+const Profile = require('../../models/Profile')
+const User = require('../../models/User')
 
-//ROUTE:    GET api/profile
-//DESC:     Test Route
-//ACCESS:   Public
-router.get('/', (req, res) => res.send('Profile Route'))
+//ROUTE:    GET api/profile/me
+//DESC:     Get current user's profile
+//ACCESS:   Private
+router.get('/me', authWare, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate('user', ['name', 'avatar'])
+
+    if (!profile) {
+      return res
+        .status(400)
+        .json({ message: 'These Credentials are no good. ' })
+    }
+    res.json(profile)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).send('Server Error')
+  }
+})
 
 module.exports = router
