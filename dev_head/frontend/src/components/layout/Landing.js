485d704a35d6d3406ca0394bd98f222b9e635c8d
hotcoffeehero <hotcoffeehero@gmail.com>
2021-02-03 06:15:55 +0900

    React Router Setup

--- dev_head/frontend/src/components/layout/Landing.js
+++ dev_head/frontend/src/components/layout/Landing.js
@@ -1,22 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Landing = () => {
   return (
     <section className='landing'>
       <div className='dark-overlay'>
         <div className='landing-inner'>
-          <h1 className='x-large'>Developer Connector</h1>
+          <h1 className='x-large'>DEV WEB</h1>
           <p className='lead'>
-            Create a developer profile/portfolio, share posts and get help from
-            other developers
+            A social media portal for you and your fellow dev heads
           </p>
           <div className='buttons'>
-            <a href='register.html' className='btn btn-primary'>
+            <Link to='/register' className='btn btn-primary'>
               Sign Up
-            </a>
-            <a href='login.html' className='btn btn-light'>
+            </Link>
+            <Link to='/login' className='btn btn-light'>
               Login
-            </a>
+            </Link>
           </div>
         </div>
       </div>
