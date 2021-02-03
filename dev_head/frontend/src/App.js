7e773c4734536a610e2af0be8ca95afcf2a82b75
hotcoffeehero <hotcoffeehero@gmail.com>
2021-02-03 17:55:47 +0900

    Frontend Set Auth Token

--- dev_head/frontend/src/App.js
+++ dev_head/frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Login from './components/auth/Login'
@@ -9,25 +9,37 @@ import Landing from './components/layout/Landing'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 import Alert from './components/layout/Alert'
+import { loadUser } from './redux/actions/auth'
+import setAuthToken from './utils/setAuthToken'
 
 import './App.css'
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path='/' component={Landing} />
-        <section className='container'>
-          <Alert />
-          <Switch>
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/register' component={Register} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
-  </Provider>
-)
+if (localStorage.token) {
+  setAuthToken(localStorage.token)
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser())
+  }, [])
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Alert />
+            <Switch>
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/register' component={Register} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  )
+}
 
 export default App
