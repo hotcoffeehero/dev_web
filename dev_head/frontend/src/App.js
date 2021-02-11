1630d49c5c3c77cdfc7d4f92f8780b24f27b51cd
hotcoffeehero <hotcoffeehero@gmail.com>
2021-02-11 15:02:24 +0900

    Add Experience & Education

--- dev_head/frontend/src/App.js
+++ dev_head/frontend/src/App.js
@@ -5,14 +5,16 @@ import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
+import Alert from './components/layout/Alert'
 import Dashboard from './components/dashboard/Dashboard'
 import CreateProfile from './components/profile-forms/CreateProfile'
 import EditProfile from './components/profile-forms/EditProfile'
+import AddExperience from './components/profile-forms/AddExperience'
+import AddEducation from './components/profile-forms/AddEducation'
 import PrivateRoute from './components/routing/PrivateRoute'
 
 import { Provider } from 'react-redux'
 import store from './redux/store'
-import Alert from './components/layout/Alert'
 import { loadUser } from './redux/actions/auth'
 import setAuthToken from './utils/setAuthToken'
 
@@ -48,6 +50,16 @@ const App = () => {
                 exact
                 path='/edit-profile'
                 component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/add-experience'
+                component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path='/add-education'
+                component={AddEducation}
               />
             </Switch>
           </section>
