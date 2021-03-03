d6a54914d13c5172861224e90636e01bf1b79479
hotcoffeehero <hotcoffeehero@gmail.com>
2021-03-03 22:36:56 +0900

    Profile Exp & Edu

--- dev_head/frontend/src/components/profile/Profile.js
+++ dev_head/frontend/src/components/profile/Profile.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import ProfileTop from './ProfileTop'
 import ProfileAbout from './ProfileAbout'
+import ProfileExperience from './ProfileExperience'
+import ProfileEducation from './ProfileEducation'
 import Spinner from '../layout/Spinner'
 import { getProfileById } from '../../redux/actions/profile'
 
@@ -36,6 +38,37 @@ const Profile = ({
           <div class='profile-grid my-1'>
             <ProfileTop profile={profile} />
             <ProfileAbout profile={Profile} />
+            <div className='profile-exp bg-white p-2'>
+              <h2 className='text-primary'>Experience</h2>
+              {profile.experience.length > 0 ? (
+                <Fragment>
+                  {profile.experience.map((experience) => (
+                    <ProfileExperience
+                      key={experience._id}
+                      experience={experience}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No Experiences Yet. </h4>
+              )}
+            </div>
+
+            <div className='profile-edu bg-white p-2'>
+              <h2 className='text-primary'>Education</h2>
+              {profile.education.length > 0 ? (
+                <Fragment>
+                  {profile.education.map((education) => (
+                    <ProfileEducation
+                      key={education._id}
+                      education={education}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No Education Info Yet. </h4>
+              )}
+            </div>
           </div>
         </Fragment>
       )}
