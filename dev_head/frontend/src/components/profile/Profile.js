1d64b091cff6c689df72fb814a12c997ccf10ee0
hotcoffeehero <hotcoffeehero@gmail.com>
2021-03-27 17:46:29 +0900

    Adding Posts

--- dev_head/frontend/src/components/profile/Profile.js
+++ dev_head/frontend/src/components/profile/Profile.js
@@ -38,7 +38,7 @@ const Profile = ({
             )}
           <div class='profile-grid my-1'>
             <ProfileTop profile={profile} />
-            <ProfileAbout profile={Profile} />
+            <ProfileAbout profile={profile} />
             <div className='profile-exp bg-white p-2'>
               <h2 className='text-primary'>Experience</h2>
               {profile.experience.length > 0 ? (
